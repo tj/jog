2306a47dd273b0a5d638b3acc065dfcd9979a9df
TJ Holowaychuk <tj@vision-media.ca>
2012-04-10 10:51:27 -0700

    tweak example

--- examples/levels.js
+++ examples/levels.js
@@ -1,11 +1,12 @@
 
 var Jog = require('..')
-  , log = new Jog(new Jog.FileStore('/tmp/levels.log'));
+  , log = new Jog(new Jog.FileStore('/tmp/levels.log'))
+  , id = 0;
 
 setInterval(function(){
-  log.info('something happened');
+  log.info('something happened', { user: 'tobi' });
 }, 1000);
 
 setInterval(function(){
-  log.error('something broke');
+  log.error('something broke', { user: 'jane', errorid: id++ });
 }, 4000);
